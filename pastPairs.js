d0771dd29c44030ef605c9885b28e1e9202e6506
Ben Ellentuck <ellentuckben@gmail.com>
2018-06-05 14:28:30 -0400

    emptied out pastPairs data

--- pastPairs.js
+++ pastPairs.js
@@ -1,18 +1,33 @@
 module.exports = {
   pastPairs: {
-    Nick: new Set(['Don']),
-    Don: new Set(['Nick']),
-    Alexa: new Set(['Marshall']),
-    Marshall: new Set(['Alexa']),
-    Yoni: new Set(['Ben']),
-    Ben: new Set(['Yoni']),
-    Rocky: new Set(['Jeff']),
-    Jeff: new Set(['Rocky']),
+    Nick: new Set(),
+    Don: new Set(),
+    Alexa: new Set(),
+    Marshall: new Set(),
+    Yoni: new Set(),
+    Ben: new Set(),
+    Rocky: new Set(),
+    Jeff: new Set(),
   },
-  mostRecentPairs: new Map([
-    ['Nick', 'Don'],
-    ['Alexa', 'Marshall'],
-    ['Yoni', 'Ben'],
-    ['Rocky', 'Jeff'],
-  ]), // keys = interviewers, vals = interviewees.
+  mostRecentPairs: new Map(), // keys = interviewers, vals = interviewees.
 };
+
+
+// module.exports = {
+//   pastPairs: {
+//     Nick: new Set(['Don']),
+//     Don: new Set(['Nick']),
+//     Alexa: new Set(['Marshall']),
+//     Marshall: new Set(['Alexa']),
+//     Yoni: new Set(['Ben']),
+//     Ben: new Set(['Yoni']),
+//     Rocky: new Set(['Jeff']),
+//     Jeff: new Set(['Rocky']),
+//   },
+//   mostRecentPairs: new Map([
+//     ['Nick', 'Don'],
+//     ['Alexa', 'Marshall'],
+//     ['Yoni', 'Ben'],
+//     ['Rocky', 'Jeff'],
+//   ]), // keys = interviewers, vals = interviewees.
+// };
